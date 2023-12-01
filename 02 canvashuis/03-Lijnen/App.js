19e5d3b5573aa31bc08f9c1ce0ebd3a4182d5f4f
Joyce <37791@ma-web.nl>
2023-12-01 10:58:37 +0100

    wwww

---
+++ 02 canvashuis/03-Lijnen/App.js
@@ -0,0 +1,33 @@
+class App
+{
+    RunApplication()
+    {
+        console.log('haiii haiii');
+        let canvas = document.getElementById("canvasId");
+        let g = canvas.getContext("2d");
+        console.log(canvas);
+
+        
+
+        
+        g.beginPath()
+        g.fillStyle = 'Yellow';
+        g.moveTo(20,20);
+        g.lineTo(300,300);
+        g.lineTo(580,20);
+        g.closePath();
+        g.stroke();
+        g.fill();
+
+
+        
+
+
+    }
+
+   
+}
+
+let app = new App();
+app.RunApplication();
+
